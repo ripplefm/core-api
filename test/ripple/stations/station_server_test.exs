debb24aa34c5398ef520ea7b9b7fb73486fc9066
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2018-11-14 18:05:22 -0500

    Add more tests for station server including state handoff

--- test/ripple/stations/station_server_test.exs
+++ test/ripple/stations/station_server_test.exs
@@ -1,31 +1,82 @@
 defmodule Ripple.StationServerTest do
   use Ripple.DataCase
 
-  alias Ripple.Stations.StationServer
+  alias Ripple.Stations.{StationServer, LiveStation, StationStore, StationHandoffStore}
 
-  describe "station server" do
-    @base_state %{
-      name: "some name",
-      slug: "some-slug",
-      tags: [],
-      guests: 0,
-      queue: [],
-      current_track: nil
-    }
-
+  describe "StationServer" do
     @track_url "https://www.youtube.com/watch?v=4Rc-NGWEHdU"
 
     setup do
       {:ok, user} = Ripple.Users.create_user(%{username: "tester"})
-      %{state: Map.put(@base_state, :users, [user]), user: user}
+
+      {:ok, station} =
+        Ripple.Stations.create_station(%{
+          creator_id: user.id,
+          tags: [],
+          play_type: "public",
+          name: "Test Station"
+        })
+
+      state = %LiveStation{
+        id: station.id,
+        name: station.name,
+        play_type: station.play_type,
+        slug: station.slug,
+        guests: 0,
+        users: [user],
+        current_track: nil,
+        queue: [],
+        creator_id: user.id
+      }
+
+      %{state: state, user: user, station: station}
     end
 
-    test "add a user to a station", %{state: state, user: user} do
+    test "init successfully creates station server for a user", %{
+      state: state,
+      station: station,
+      user: user
+    } do
+      assert {:ok, result_station} = StationServer.init({station, user})
+      assert result_station == state
+    end
+
+    test "init succesfully creates station server for a guest", %{state: state, station: station} do
+      assert {:ok, result_station} = StationServer.init({station, nil})
+      assert result_station == %LiveStation{state | users: [], guests: 1}
+    end
+
+    test "init successfully retrieves station from handoff store", %{
+      state: state,
+      station: station,
+      user: user
+    } do
+      initial_state = %LiveStation{state | tags: ["test"]}
+      assert :ok == StationHandoffStore.put(initial_state)
+      assert {:ok, result_station} = StationServer.init({station, user})
+      assert result_station == %LiveStation{initial_state | users: []}
+    end
+
+    test "init successfully saves state to handoff store when exitting", %{state: state} do
+      assert :mnesia.table_info(StationHandoffStore, :size) == 0
+      assert :ok = StationServer.terminate(:exit, state)
+      assert [{_, _, saved_state, _}] = :mnesia.dirty_read(StationHandoffStore, state.slug)
+      assert saved_state == state
+    end
+
+    test "add a new user to a station", %{state: state, user: user} do
       {:ok, new_user} = Ripple.Users.create_user(%{username: "tester2"})
       {:reply, :ok, new_state} = StationServer.handle_call({:add_user, new_user}, self(), state)
-      assert state.users == [user]
-      assert new_state.users == [user, new_user]
-      assert Map.delete(new_state, :users) == Map.delete(state, :users)
+      assert new_state == %LiveStation{state | users: [user, new_user]}
+    end
+
+    test "adding a duplicate user to a station does nothing", %{state: state, user: user} do
+      initial_state = Map.put(state, :users, [user])
+
+      assert {:reply, :ok, returned_state} =
+               StationServer.handle_call({:add_user, user}, self(), initial_state)
+
+      assert returned_state == initial_state
     end
 
     test "add a guest to a station", %{state: state} do
@@ -42,23 +93,91 @@ defmodule Ripple.StationServerTest do
       assert Map.delete(new_state, :users) == Map.delete(state, :users)
     end
 
-    test "remove a guest from a station with 1 guest", %{state: state, user: user} do
-      {:reply, :ok, state_with_guest} = StationServer.handle_call({:add_user, nil}, self(), state)
-      assert state_with_guest.users == [user]
-      assert state_with_guest.guests == 1
+    test "remove a guest from a station with 1 guest", %{state: state} do
+      initial_state = %LiveStation{state | users: [], guests: 1}
 
-      {:reply, false, state_with_guest_without_user} =
-        StationServer.handle_call({:remove_user, user}, self(), state_with_guest)
+      assert {:reply, true, new_state} =
+               StationServer.handle_call({:remove_user, nil}, self(), initial_state)
 
-      assert state_with_guest_without_user.users == []
-      assert state_with_guest_without_user.guests == 1
+      assert new_state == %LiveStation{initial_state | guests: 0}
+    end
 
-      {:reply, true, new_state} =
-        StationServer.handle_call({:remove_user, nil}, self(), state_with_guest_without_user)
+    test "remove the last user from a station with guests", %{state: state, user: user} do
+      initial_state = %LiveStation{state | guests: 2}
 
-      assert new_state.users == []
-      assert new_state.guests == 0
-      assert Map.delete(new_state, :users) == Map.delete(state, :users)
+      assert {:reply, false, new_state} =
+               StationServer.handle_call({:remove_user, user}, self(), initial_state)
+
+      assert new_state == %LiveStation{initial_state | users: []}
+    end
+
+    test "remove the last guest from a station with users", %{state: state} do
+      initial_state = %LiveStation{state | guests: 1}
+
+      assert {:reply, false, new_state} =
+               StationServer.handle_call({:remove_user, nil}, self(), initial_state)
+
+      assert new_state == %LiveStation{initial_state | guests: 0}
+    end
+
+    test "state not saved to handoff when exit called by server", %{state: state, user: user} do
+      assert {:reply, true, new_state} =
+               StationServer.handle_call({:remove_user, user}, self(), state)
+
+      assert :mnesia.table_info(StationHandoffStore, :size) == 0
+    end
+
+    test "station removed from station store when exit called by server", %{
+      state: state,
+      user: user
+    } do
+      assert {:reply, true, new_state} =
+               StationServer.handle_call({:remove_user, user}, self(), state)
+
+      assert StationStore.read(state.slug) == {:ok, nil}
+    end
+
+    test "station with private play_type does not allow random user to play tracks", %{
+      state: state,
+      user: user
+    } do
+      {:ok, new_user} = Ripple.Users.create_user(%{username: "tester2"})
+      initial_state = %LiveStation{state | play_type: "private", users: [user, new_user]}
+
+      assert {:reply, {:error, :not_creator}, returned_state} =
+               StationServer.handle_cast({:add_track, @track_url, new_user}, initial_state)
+
+      assert returned_state == initial_state
+    end
+
+    test "station with private play_type allows creator to play tracks", %{
+      state: state,
+      user: user
+    } do
+      initial_state = %LiveStation{state | play_type: "private"}
+
+      assert {:noreply, returned_state} =
+               StationServer.handle_cast({:add_track, @track_url, user}, initial_state)
+
+      assert returned_state.current_track.url == @track_url
+      assert %LiveStation{returned_state | current_track: nil} == initial_state
+    end
+
+    test "adding a track fails if user not in station", %{state: state, user: user} do
+      initial_state = %LiveStation{state | users: []}
+
+      assert {:reply, {:error, :not_in_station}, new_state} =
+               StationServer.handle_cast({:add_track, @track_url, user}, initial_state)
+
+      assert new_state == initial_state
+    end
+
+    test "adding a track fails if guest is passed in", %{state: state} do
+      assert_raise FunctionClauseError,
+                   "no function clause matching in Ripple.Stations.StationServer.handle_cast/2",
+                   fn ->
+                     StationServer.handle_cast({:add_track, @track_url, nil}, state)
+                   end
     end
 
     test "add a track to a station with no track", %{state: state, user: user} do
