d1494b4e49c5b61f619d0c33652237f44e3d62c3
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2019-02-25 03:37:45 -0500

    Add extra resources (i.e playlists, users) for provider api clients

--- lib/ripple/tracks/providers/soundcloud.ex
+++ lib/ripple/tracks/providers/soundcloud.ex
@@ -10,15 +10,41 @@ defmodule Ripple.Tracks.Providers.SoundCloud do
     |> parse_track(url)
   end
 
-  defp make_request(url) do
+  def make_request(url) do
     {:ok, res} =
       HTTPoison.get(
-        "https://api.soundcloud.com/resolve?url=#{url}&client_id=#{api_key()}",
+        "https://api.soundcloud.com/resolve?url=#{url}&client_id=#{api_key()}&limit=100",
         [],
         follow_redirect: true
       )
 
     res
+  end
+
+  def get_related_tracks(url) do
+    track_uri = url |> make_request |> process_body |> Map.get("uri")
+
+    {:ok, res} =
+      HTTPoison.get(
+        "#{track_uri}/related?client_id=#{api_key()}&limit=100",
+        [],
+        follow_redirect: true
+      )
+
+    res.body |> Poison.decode!()
+  end
+
+  def get_user_tracks(url) do
+    make_request("#{url}/tracks") |> process_body
+  end
+
+  def get_playlist_tracks(url) do
+    url |> make_request |> process_body |> Map.get("tracks")
+  end
+
+  def get_chart_tracks(url) do
+    {:ok, res} = HTTPoison.get(url <> "&client_id=#{api_key()}")
+    process_body(res) |> Map.get("collection") |> Enum.map(&Map.get(&1, "track"))
   end
 
   defp process_body(res) do
