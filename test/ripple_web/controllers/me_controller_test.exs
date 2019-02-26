8d6dbfb8f862eafc948225410e97bae05a784244
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2019-02-25 22:30:25 -0500

    Add tests for created and followed by playlists functionality

--- test/ripple_web/controllers/me_controller_test.exs
+++ test/ripple_web/controllers/me_controller_test.exs
@@ -2,10 +2,11 @@ defmodule RippleWeb.MeControllerTest do
   use RippleWeb.ConnCase
 
   alias Ripple.Stations
+  alias Ripple.Playlists
 
   @create_attrs %{name: "Some station", tags: [], visibility: "public"}
 
-  def station_fixture(overrides \\ %{}) do
+  def station_fixture() do
     {:ok, user} =
       case Ripple.Users.get_user("tester") do
         {:error, :not_found} -> Ripple.Users.create_user(%{username: "tester"})
@@ -13,11 +14,29 @@ defmodule RippleWeb.MeControllerTest do
       end
 
     {:ok, station} =
-      Map.merge(@create_attrs, overrides)
+      @create_attrs
       |> Map.put(:creator_id, user.id)
       |> Stations.create_station()
 
     station
+  end
+
+  def playlist_fixture() do
+    {:ok, user} =
+      case Ripple.Users.get_user("tester") do
+        {:error, :not_found} -> Ripple.Users.create_user(%{username: "tester"})
+        u -> {:ok, u}
+      end
+
+    {:ok, playlist} =
+      Playlists.create_playlist(%{
+        creator_id: user.id,
+        name: "some playlist",
+        tags: [],
+        visibility: "public"
+      })
+
+    playlist
   end
 
   def token_fixture do
@@ -89,6 +108,72 @@ defmodule RippleWeb.MeControllerTest do
       conn = conn |> get(me_path(conn, :show_following_stations))
 
       assert json_response(conn, 200)["stations"] != []
+    end
+  end
+
+  describe "show_created_playlists" do
+    test "401 when missing authorization header", %{conn: conn} do
+      conn = get(conn, me_path(conn, :show_created_playlists))
+      assert conn.status == 401
+      assert(conn.assigns.current_user == nil)
+    end
+
+    test "401 when token missing 'playlists:read' scope", %{conn: conn} do
+      token = token_fixture()
+
+      conn =
+        conn
+        |> put_req_header("authorization", "Bearer #{token}")
+        |> get(me_path(conn, :show_created_playlists))
+
+      assert ["Bearer #{token}"] == get_req_header(conn, "authorization")
+
+      assert json_response(conn, 401) == %{
+               "errors" => %{"detail" => "Invalid scopes for resource."}
+             }
+    end
+
+    @tag :authenticated
+    test "returns created playlists when authenticated", %{conn: conn} do
+      user = Ripple.Users.get_user("tester")
+      playlist = playlist_fixture()
+      {:ok, _} = Playlists.follow_playlist(playlist, user)
+      conn = conn |> get(me_path(conn, :show_created_playlists))
+
+      assert json_response(conn, 200)["playlists"] != []
+    end
+  end
+
+  describe "show_following_playlists" do
+    test "401 when missing authorization header", %{conn: conn} do
+      conn = get(conn, me_path(conn, :show_following_playlists))
+      assert conn.status == 401
+      assert(conn.assigns.current_user == nil)
+    end
+
+    test "401 when token missing 'playlists:read' scope", %{conn: conn} do
+      token = token_fixture()
+
+      conn =
+        conn
+        |> put_req_header("authorization", "Bearer #{token}")
+        |> get(me_path(conn, :show_following_playlists))
+
+      assert ["Bearer #{token}"] == get_req_header(conn, "authorization")
+
+      assert json_response(conn, 401) == %{
+               "errors" => %{"detail" => "Invalid scopes for resource."}
+             }
+    end
+
+    @tag :authenticated
+    test "returns following playlists when authenticated", %{conn: conn} do
+      user = Ripple.Users.get_user("tester")
+      playlist = playlist_fixture()
+      {:ok, _} = Playlists.follow_playlist(playlist, user)
+      conn = conn |> get(me_path(conn, :show_following_playlists))
+
+      assert json_response(conn, 200)["playlists"] != []
     end
   end
 end
